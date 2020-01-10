d0c19728ac27d94641fc79fade153d240e7766e0
Noel-elon <noelnwaelugo@gmail.com>
2020-01-10 09:32:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/navigation/TitleFragment.kt
@@ -0,0 +1,30 @@
+package com.example.navigation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import com.example.navigation.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //Inflating the fragment XML... also using databinding
+val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        return  binding.root
+        }
+    }
+
+
+
